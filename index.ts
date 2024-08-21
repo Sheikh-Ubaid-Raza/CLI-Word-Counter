6d223478e7acc0a40d221371ff34feedee03a25d
unknown <shekhubaidraza76@gmail.com>
2024-08-21 12:28:03 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([{
+    name:"sentence",
+    message:"Enter Your Sentence to counts the words!",
+    type:"input"
+}])
+
+const words = answer.sentence.trim().split(" "); // trim => Remove White Spaces from start & end
+console.log(words);
+
+console.log(`Your Sentence word count is ${words.length}`);
